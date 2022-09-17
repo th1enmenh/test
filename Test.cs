f342ccd9d10c01a6c7010b5ff181af9a640aeb34
th1enmenh <ldphong99@gmail.com>
2022-09-17 14:36:23 +0700

    test 2

---
+++ Test.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testgit
+{
+    public class Test
+    {
+        int a = 1;
+        string a = "abc";
+    }
+}
